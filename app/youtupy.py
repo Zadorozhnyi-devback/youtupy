6ad86bf999bb4421120c19c092858b2f8bfa8b29
Monster <BreakMonster95@gmail.com>
2024-07-05 09:28:20 +0300

    fix pytube in running

--- app/youtupy.py
+++ app/youtupy.py
@@ -1,7 +1,13 @@
+import pytube
 import os
 
 from ui_tkinter.tkin import YouTupy
 
 if __name__ == "__main__":
+    # fixing bug
+    pytube.innertube._default_clients['ANDROID'] = (
+        pytube.innertube._default_clients['WEB']
+    )
+
     entry_point_path = os.path.dirname(os.path.realpath(__file__))
     YouTupy(entry_point_path=entry_point_path)
